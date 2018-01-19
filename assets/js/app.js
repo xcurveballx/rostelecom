9874e8c614709ece808a99a9934688037bb403e2
curveball <x.curveball.x@gmail.com>
2018-01-20 01:07:47 +0300

    Loading JSON

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,33 @@
-import LoadManager from './loadManager';
+"use strict";
+import ObserverList from './ObserverList';
+import LoadManagerProxy from './LoadManagerProxy';
+import StorageManager from './StorageManager';
 
-let person = new LoadManager('John Doe');
-person.sayHi();
+let loadManager = new LoadManagerProxy(new ObserverList());
+let storageManager = new StorageManager('rostelecom');
+
+const config = [{
+    instance: loadManager,
+    notifies: [{
+      name: 'test',
+      observers: [{
+        instance: storageManager,
+        callback: storageManager.doSomething
+      }]
+    }]
+}];
+
+config.forEach(subject => {
+  subject.notifies.forEach(evt => {
+    evt.observers.forEach(observer => {
+      subject.instance.addObserver(evt.name, observer.instance, observer.callback);
+    });
+  });
+});
+
+let promise = loadManager.load();
+
+Promise.all([promise]).then(
+    result => loadManager.notify('test', result),
+    error => loadManager.notify('test', error)
+);
--- assets/js/loadManager.js
+++ assets/js/loadManager.js
@@ -1,8 +1,13 @@
 export default class LoadManager {
-  constructor(name) {
-    this.name = name;
+  constructor(ObserverList) {
+    this.observers = ObserverList;
+    this.url = 'http://81.177.101.143:30080/test.json';
   }
-  sayHi() {
-    //console.log(this.name);
+  // Subject methods
+  addObserver(evt, observer, callback) {
+    this.observers.add(evt, observer, callback);
+  }
+  notify(evt, data) {
+    this.observers.list[evt].forEach(observer => observer.instance.update(observer.callback, data) );
   }
 }
